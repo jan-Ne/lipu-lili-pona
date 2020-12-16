118f3072fc08eb19308557d651d98c8aa22be3ea
jan Ne <46408244+jan-Ne@users.noreply.github.com>
2020-12-16 18:56:15 -0500

    Update langmenu.js

--- langmenu.js
+++ langmenu.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/>English</option>");
-  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/id>Bahasa Indonesia</option>")
-  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/es>Español</option>");
-  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/fr>Français</option>");
-  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/pt>Português</option>");
-  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/ru>Русский</option>");
+  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/'>English</option>");
+  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/id'>Bahasa Indonesia</option>")
+  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/es'>Español</option>");
+  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/fr'>Français</option>");
+  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/pt'>Português</option>");
+  $("#lang-select").append("<option value='https://jan-ne.github.io/lipu-lili-pona/ru'>Русский</option>");
 });
   
