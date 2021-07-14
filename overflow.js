7b04dc0255578be55f7a5a8944c59240265a7b02
jan Ne <46408244+jan-Ne@users.noreply.github.com>
2021-07-14 15:41:16 -0400

    Create overflow.js

---
+++ overflow.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".dictionary p").each(function() {
+    var overflowAmt = this.scrollWidth - this.clientWidth;
+    var size = parseFloat($(this).css("font-size"));
+    if (overflowAmt > 0){
+      $(this).css("font-size", size - (overflowAmt * 0.1));
+      if (overflowAmt > 0){
+        var size = parseFloat($(this).css("font-size"));
+        $(this).css("font-size", Math.floor((size - 0.1)*10)/10);
+      }
+    }
+  });
+}
