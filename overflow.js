919964f1aa8805aebc58057adecb6bb10d044899
jan Ne <46408244+jan-Ne@users.noreply.github.com>
2021-07-14 15:49:47 -0400

    Update overflow.js

--- overflow.js
+++ overflow.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(window).on( "load", function() {
   $(".dictionary p").each(function() {
     var overflowAmt = this.scrollWidth - this.clientWidth;
     var size = parseFloat($(this).css("font-size"));
